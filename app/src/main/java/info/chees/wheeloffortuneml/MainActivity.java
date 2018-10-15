21f6cbca2dcc52d8ce8aad324c4712aded28116d
Christiaan Hees <mail@chees.info>
2018-10-15 12:01:13 +0200

    Detect start and stop of spinning

--- app/src/main/java/info/chees/wheeloffortuneml/MainActivity.java
+++ app/src/main/java/info/chees/wheeloffortuneml/MainActivity.java
@@ -5,6 +5,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -13,7 +14,8 @@ import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager mSensorManager;
-    private TextView textView;
+    private TextView currentTextView;
+    private TextView historyTextView;
 
     private final float[] mAccelerometerReading = new float[3];
     private final float[] mMagnetometerReading = new float[3];
@@ -21,13 +23,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private final float[] mRotationMatrix = new float[9];
     private final float[] mOrientationAngles = new float[3];
 
+    private Handler handler;
+    private boolean running;
+
+    private float previousAngle;
+    private boolean spinning;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.output);
+        currentTextView = findViewById(R.id.current);
+        historyTextView = findViewById(R.id.history);
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
     }
@@ -46,18 +55,62 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Sensor accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         // TODO lower delay?
         if (accelerometer != null) {
-            mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+            mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
         }
         Sensor magneticField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         if (magneticField != null) {
-            mSensorManager.registerListener(this, magneticField, SensorManager.SENSOR_DELAY_NORMAL);
+            mSensorManager.registerListener(this, magneticField, SensorManager.SENSOR_DELAY_GAME);
         }
+
+        running = true;
+
+        handler = new Handler();
+        final int delay = 100; //milliseconds
+
+        handler.postDelayed(new Runnable() {
+            public void run(){
+                updateOrientationAngles();
+
+                float angleDiff = Math.abs(mOrientationAngles[0] - previousAngle);
+
+                currentTextView.setText("" + round(mOrientationAngles[0], 2));
+                currentTextView.setRotation((float) Math.toDegrees(-mOrientationAngles[0]));
+                //currentTextView.setText("" + round(angleDiff, 3));
+
+                if (angleDiff < 0.04) {
+                    // Standing still
+                    if (spinning) {
+                        // Just stopped
+                        historyTextView.append("\nDone");
+                        spinning = false;
+                    }
+                } else {
+                    // Spinning
+                    if (!spinning) {
+                        // Just started
+                        if (angleDiff > 0.4) {
+                            // Only start when the angle got big enough
+                            historyTextView.setText("Start");
+                            spinning = true;
+                        }
+                    } else {
+                        historyTextView.append("\n" + round(mOrientationAngles[0], 2));
+                    }
+                }
+                previousAngle = mOrientationAngles[0];
+
+                if (running) {
+                    handler.postDelayed(this, delay);
+                }
+            }
+        }, delay);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         mSensorManager.unregisterListener(this);
+        running = false;
     }
 
 
@@ -72,8 +125,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             System.arraycopy(event.values, 0, mMagnetometerReading,
                     0, mMagnetometerReading.length);
         }
-
-        updateOrientationAngles();
     }
 
     public void updateOrientationAngles() {
@@ -86,10 +137,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         SensorManager.getOrientation(mRotationMatrix, mOrientationAngles);
 
         // "mOrientationAngles" now has up-to-date information.
-
-        textView.setText("Yaw: " + round(mOrientationAngles[0], 2) +
-                "\n Pitch: " + round(mOrientationAngles[1], 2) +
-                "\n Roll: " + round(mOrientationAngles[2], 2));
     }
 
     public static float round(float d, int decimalPlace) {
